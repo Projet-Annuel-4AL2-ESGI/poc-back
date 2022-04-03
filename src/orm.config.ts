7f165b65f9dd2714c695a28a116a535a9edd49fc
milutinstanojevic <milutinstan@outlook.fr>
2022-04-03 21:49:05 +0200

    make it better

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,11 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  username: 'postgres',
-  password: 'admin',
+  username: 'fjwufefmvafgrc',
+  password: 'a5b909c3442fdcc46150a8fd8ee99bb35022a932c0195a538c66f65b1db2a2e7',
   port: 5432,
-  host: '127.0.0.1',
-  database: 'poc_back',
+  host: 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
+  database: 'd6gg3357g0fds0',
   synchronize: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
 };
