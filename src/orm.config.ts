b5d3c8e0acd04cbc17796537a72076d81e31747c
Ad <adrienroux94@gmail.com>
2022-04-04 12:02:39 +0200

    .env added gitignore config for db URL

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,12 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// export const config: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   username: 'fjwufefmvafgrc',
+//   password: 'a5b909c3442fdcc46150a8fd8ee99bb35022a932c0195a538c66f65b1db2a2e7',
+//   port: 5432,
+//   host: 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
+//   database: 'd6gg3357g0fds0',
+//   synchronize: true,
+//   ssl: { rejectUnauthorized: false },
+//   entities: ['dist/**/*.entity{.ts,.js}'],
+// };
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  username: 'fjwufefmvafgrc',
-  password: 'a5b909c3442fdcc46150a8fd8ee99bb35022a932c0195a538c66f65b1db2a2e7',
-  port: 5432,
-  host: 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
-  database: 'd6gg3357g0fds0',
-  synchronize: true,
-  ssl: { rejectUnauthorized: false },
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  url: process.env.DATABASE_URL,
 };
