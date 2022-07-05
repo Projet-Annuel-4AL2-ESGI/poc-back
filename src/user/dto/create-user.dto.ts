d95b4c3d48000cf053791f98079b30cc1c3dde2b
halimatou-cpu <halimatoubah.bh@gmail.com>
2022-07-05 13:58:24 +0200

    swagge doc

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+
   id?: number;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   username?: string;
+
+  @ApiProperty()
   password: string;
+
+  @ApiProperty()
   followers?: number[];
+
+  @ApiProperty()
   image?: string;
 }
