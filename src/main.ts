50c7d16bd3386104153bcd5a8da1caf59801bbb9
Ad <adrienroux94@gmail.com>
2022-07-27 14:58:19 +0200

    image upload fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(bodyParser.json({ limit: '3mb' }));
-  app.use(bodyParser.urlencoded({ limit: '3mb' }));
+  app.use(bodyParser.json({ limit: '7mb' }));
+  app.use(bodyParser.urlencoded({ limit: '7mb' }));
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
